c3a0b401b1416b46d062f7f74c81e995d7aac122
avantika-rawat <avantika.202104080@tulas.edu.in>
2025-05-01 22:46:42 +0530

    commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
     <div
-      className="h-60 bg-cover text-white flex justify-center items-center "
-      style={{ backgroundImage: `url(/images/footer-bg.jpg)` }}
+      className="h-60 bg-cover text-white flex justify-center items-center  "
+      style={{ backgroundImage: `url(/images/footer-background.png)` }}
     >
       {" "}
-      <h1 className="text-xl font-semibold text-center ml-4 ">
+      <h1 className="text-xl font-semibold text-center ">
         &copy; 2024, Made with Love By Avantika Rawat
       </h1>
     </div>
